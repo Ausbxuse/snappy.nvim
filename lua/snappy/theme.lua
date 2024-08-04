3c181a8d693011bcb56dbff08d7213e0072dd21e
Zhenyu Zhao <peterzhaozzy@gmail.com>
2024-08-05 01:18:34 +0800

    better line color

--- lua/snappy/theme.lua
+++ lua/snappy/theme.lua
@@ -37,7 +37,7 @@ function M.setup()
 		CursorColumn = { bg = c.bg_highlight }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
 		CursorLine = { bg = c.bg_highlight }, -- Screen-line at the cursor, when 'cursorline' is set.  Low-priority if foreground (ctermfg OR guifg) is not set.
 		LineNr = { fg = c.dark3 }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
-		CursorLineNr = { fg = c.fg_dark }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
+		CursorLineNr = { fg = c.orange, bold = true }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
 		Directory = { fg = c.blue }, -- directory names (and other special names in listings)
 		DiffAdd = { bg = c.diff.add }, -- diff mode: Added line |diff.txt|
 		DiffChange = { bg = c.diff.change }, -- diff mode: Changed line |diff.txt|
